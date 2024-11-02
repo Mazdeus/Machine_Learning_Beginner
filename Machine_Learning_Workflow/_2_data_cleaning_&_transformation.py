ac38af08c1a829b0d94cc2d368d342e3258b294f
Mazdeus <fadhilcr1@gmail.com>
2024-11-02 14:01:41 +0700

    Update data cleaning & transformation

--- Machine_Learning_Workflow/_2_data_cleaning_&_transformation.py
+++ Machine_Learning_Workflow/_2_data_cleaning_&_transformation.py
@@ -106,8 +106,78 @@ for feature in numeric_features:
     # Menggunakan lambda untuk mengganti nilai outliers dengan median
     df[feature] = df[feature].apply(lambda x: median[feature] if x < (Q1[feature] - 1.5 * IQR[feature]) or x > (Q3[feature] + 1.5 * IQR[feature]) else x)
 
-for feature in numeric_features:
-    plt.figure(figsize=(10,6))
-    sns.boxplot(x=df[feature])
-    plt.title(f'Box Plot of {feature}')
-    plt.show()
+# for feature in numeric_features:
+#     plt.figure(figsize=(10,6))
+#     sns.boxplot(x=df[feature])
+#     plt.title(f'Box Plot of {feature}')
+#     plt.show()
+
+"""
+Normalisasi dan standarisasi data
+"""
+
+from sklearn.preprocessing import StandardScaler
+
+# Standarisasi fitur numerik
+scaler = StandardScaler()
+df[numeric_features] = scaler.fit_transform(df[numeric_features])
+
+# Histogram sebelum standarisasi
+plt.figure(figsize=(12,5))
+# plt.subplot(1,2,1)
+# sns.histplot(train[numeric_features[3]], kde=True)
+# plt.title("Histogram Sebelum Standarisasi")
+# plt.show()
+
+# Histogram setelah standarisasi
+plt.subplot(1,2,2)
+sns.histplot(df[numeric_features[3]], kde=True)
+plt.title("Histogram Setelah Standarisasi")
+# plt.show()
+
+"""
+Menangani duplikasi data
+"""
+
+# Mengidentifikasi baris duplikat
+duplicates = df.duplicated()
+
+# print("Baris duplikat")
+# print(df[duplicates])
+
+# Sayangnya tidak ada duplikasi, tetapi jika kasusnya selain ini dan terdapat duplikasi :
+# df = df.drop_duplicates()
+
+# print("Dataframe setelahmenghapus duplikat:")
+# print(df)
+
+"""
+Mengonversi Tipe Data
+Model machine learning tidak dapat langsung menerima input kategorikal.
+Hal ini karena mereka bergantung pada operasi matematika yang memerlukan input numerik.
+Ada 3 cara untuk menangani hal ini :
+    1. One-Hot Encoding
+    2. Label Encoding
+    3. Ordinal Encoding
+"""
+
+# Cek Data kategorikal
+category_features = df.select_dtypes(include=['object']).columns
+df[category_features]
+
+# One-Hot Encoding
+df_one_hot = pd.get_dummies(df, columns=category_features)
+print(df_one_hot)
+
+# Label Encoding
+from sklearn.preprocessing import LabelEncoder
+
+# Inisialisasi LabelEncoder
+label_encoder = LabelEncoder()
+df_lencoder = pd. DataFrame(df)
+
+for col in category_features:
+    df_lencoder[col] = label_encoder.fit_transform(df[col])
+
+# Menampilkan hasil
+print(df_lencoder)
